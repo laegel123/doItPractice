51187c231fbbdc5604973ed5c7a2da51e19c062d
laegel <laegel@selper.co.kr>
2022-12-13 11:22:45 +0900

    Title : part 12 실습

---
+++ part12/part12_1/GenericPrinterTest.java
@@ -0,0 +1,16 @@
+package part12_1;
+
+public class GenericPrinterTest {
+    public static void main(String[] args) {
+        GenericPrinter<Powder> powderPrinter = new GenericPrinter<>();
+
+        powderPrinter.setMaterial(new Powder());
+        Powder powder = powderPrinter.getMaterial();
+        System.out.println(powderPrinter);
+
+        GenericPrinter<Plastic> plasticPrinter = new GenericPrinter<>();
+        plasticPrinter.setMaterial(new Plastic());
+        Plastic plastic = plasticPrinter.getMaterial();
+        System.out.println(plasticPrinter);
+    }
+}
---
+++ part12/part12_1/Powder.java
@@ -0,0 +1,11 @@
+package part12_1;
+
+public class Powder extends Material {
+    public void doPrinting() {
+        System.out.println("Powder 재료로 출력합니다.");
+    }
+
+    public String toString() {
+        return "재료는 Powder 입니다.";
+    }
+}

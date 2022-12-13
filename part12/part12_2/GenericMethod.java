9db6c832f1fce422dd586b7b0d34032233b825d9
laegel <laegel@selper.co.kr>
2022-12-13 12:10:51 +0900

    Title : part 12 실습

---
+++ part12/part12_2/GenericMethod.java
@@ -0,0 +1,24 @@
+package part12_2;
+
+public class GenericMethod {
+    public static <T, V> double makeRectangle(Point<T, V> p1, Point<T, V> p2) {
+        double left = ((Number) p1.getX()).doubleValue();
+        double right = ((Number) p2.getX()).doubleValue();
+        double top = ((Number) p1.getY()).doubleValue();
+        double bottom = ((Number) p2.getY()).doubleValue();
+
+        double width = Math.abs(right - left);
+        double height = Math.abs(top - bottom);
+
+        return width * height;
+    }
+
+    public static void main(String[] args) {
+        Point<Integer, Double> p1 = new Point<>(0, 0.0);
+        Point<Integer, Double> p2 = new Point<>(10, 10.0);
+
+        double rect = GenericMethod.makeRectangle(p1, p2);
+
+        System.out.println(rect);
+    }
+}

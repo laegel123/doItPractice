c14a378cda99734945b7e7097cd08ab9126efa60
laegel <laegel1@gmail.com>
2022-12-27 15:06:33 +0900

    Title : part 15 실습

---
+++ part15/part15_2/FileInputStreamTest1.java
@@ -0,0 +1,32 @@
+package part15_2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInputStreamTest1 {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+
+        try {
+            fis = new FileInputStream("input.txt");
+            System.out.println(fis.read());
+            System.out.println(fis.read());
+            System.out.println(fis.read());
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            } catch (NullPointerException e) {
+                System.out.println(e);
+            }
+        }
+
+        System.out.println("end");
+    }
+}
